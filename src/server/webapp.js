b79d7704a69426dca5b3fcbba5d73084ca4ae709
Thomas Crowe <wtcrowe4@outlook.com>
2024-12-20 13:04:57 -0500

    Started to add navigation for multiple forms

--- src/server/webapp.js
+++ src/server/webapp.js
@@ -1,7 +1,28 @@
-export const doGet = () => {
-  const title = 'Google Apps Script';
-  const fileName = 'index.html';
-  return HtmlService.createHtmlOutputFromFile(fileName)
-    .setTitle(title)
-    .setXFrameOptionsMode(HtmlService.XFrameOptionsMode.DEFAULT);
+// WebApp navigation
+export const doGet = (e) => {
+  const page = e.parameter.page || 'home';
+  let fileName = 'home.html';
+  let title = 'Wand Inventory Management';
+
+  if (page === 'premade') {
+    fileName = 'premade.html';
+    title = 'Premade Inventory Management';
+  } else if (page === 'page2') {
+    fileName = 'page2.html';
+    title = 'Page 2';
+  }
+
+  // Pass the function, not just a string
+  const template = HtmlService.createTemplateFromFile(fileName);
+  template.getScriptUrl = () => ScriptApp.getService().getUrl();
+
+  return template.evaluate().setTitle(title).setXFrameOptionsMode(HtmlService.XFrameOptionsMode.DEFAULT);
+};
+
+export const include = (filename) => {
+  return HtmlService.createHtmlOutputFromFile(filename).getContent();
+};
+
+export const getScriptUrl = () => {
+  return ScriptApp.getService().getUrl();
 };
